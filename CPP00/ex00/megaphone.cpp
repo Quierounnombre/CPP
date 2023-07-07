0a8d0663d5b261b6a62812a7414dab52fc03f702
Vicente Garcia Andrade <vicgarci@c1r7s3.42madrid.com>
2023-07-07 18:15:20 +0200

    CPP 00 ex00 done

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include "megaphone.hpp"
+
+int main(int argc, char **argv)
+{
+	std::string	str;
+
+	argv++;
+	if (*argv)
+	{
+		while (*argv)
+		{
+			str = *argv;
+			std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+			std::cout << str;
+			argv++;
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
+}
