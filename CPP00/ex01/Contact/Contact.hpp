00b0b86824bc556122c217eb3c4fc0e421fb031d
Vicente Garcia Andrade <vicgarci@c1r9s1.42madrid.com>
2023-07-11 14:19:24 +0200

    Contact listo

--- CPP00/ex01/Contact/Contact.hpp
+++ CPP00/ex01/Contact/Contact.hpp
@@ -13,11 +13,24 @@ class Contact
 		Contact(const Contact &Contact);
 		~Contact();
 		Contact & operator = (const Contact &Contact);
+
+		//--------------------------------------------
+		//GETTERS
+
 		std::string	get_first_name();
 		std::string	get_last_name();
 		std::string	get_nickname();
 		int			get_phone_number();
 		std::string	get_dark_thingy();
+
+		//--------------------------------------------
+		//SETTERS
+
+		void		set_first_name(std::string	str);
+		void		set_last_name(std::string	str);
+		void		set_nickname(std::string	str);
+		void		set_phonenumber(int			num);
+		void		set_dark_thingy(std::string	str);
 
 	private:
 		std::string	_first_name;
