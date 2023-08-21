b3b6c1e9f658c68cc304ccf7a49ea6f2e787a329
root <root@DESKTOP-AHR157M.localdomain>
2023-08-21 11:38:49 +0200

    La lio con makefile(relink) y terminado ex01

--- CPP01/ex01/Zombie/zombieHorde.cpp
+++ CPP01/ex01/Zombie/zombieHorde.cpp
@@ -2,15 +2,17 @@
 
 Zombie	*Zombie::zombieHorde(int N, std::string name)
 {
+	int		i;
+	
+	i = 0;
 	if (N > 0)
 	{
 		Zombie	*Z = new Zombie[N];
-		int		i;
-
-
-		i = 0;
 		while (i < N)
+		{
 			Z[i]._name = name;
+			i++;
+		}
 		return (Z);
 	}
 	return (NULL);
--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -4,15 +4,30 @@
 static void		leakss()
 {
 	system ("leaks Plant_VS_zombies");
-}
-*/
-	//atexit(leakss);
+}*/
+
 
 int	main()
 {
 	Zombie	*Z;
+	Zombie	*Z_horde;
+	int		i;
 
-	Z->newZombie("Z");
+	//atexit(leakss);
+	i = 10;
+	Z = Z->newZombie("Z");
 	Z->randomChump("ASCA");
+	Z_horde = Z_horde->zombieHorde(i, "Ejecutivo_deprimido");
+	while (i)
+	{
+		Z_horde[i - 1].announce();
+		i--;
+	}
 	delete (Z);
+	i = 10;
+	while (i)
+	{
+		Z_horde[i - 1].~Zombie();
+		i--;
+	}
 }
