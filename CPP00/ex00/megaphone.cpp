6e2891f4b795b6085962dfbea9d0d12283e0c50c
Vicente Garcia Andrade <vicgarci@c2r4s4.42madrid.com>
2023-11-02 15:55:37 +0100

    PATATA cpp00

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,4 +1,5 @@
-#include "megaphone.hpp"
+# include <iostream>
+# include <string>
 
 int main(int argc, char **argv)
 {
--- CPP00/ex00/megaphone.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef MEGAPHONE_HPP
-# define MEGAPHONE_HPP
-
-# include <iostream>
-# include <string>
-
-#endif
