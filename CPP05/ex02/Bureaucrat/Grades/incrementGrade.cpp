cc461e121695f28a338c0c3a9fb5713a35bcac35
Vicente Garcia Andrade <vicgarci@c1r6s5.42madrid.com>
2024-02-26 17:32:05 +0100

    Bug increment grade

--- CPP05/ex02/Bureaucrat/Grades/incrementGrade.cpp
+++ CPP05/ex02/Bureaucrat/Grades/incrementGrade.cpp
@@ -2,7 +2,7 @@
 
 void	Bureaucrat::incrementGrade(n_int grade_change)
 {
-	if (check_grade_change(grade_change))
+	if (check_grade_change(-grade_change))
 		this->_grade -= grade_change;
 	else
 		throw GradeTooHighException(this->_grade - grade_change);
